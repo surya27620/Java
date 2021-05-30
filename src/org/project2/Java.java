bf32d00f8602f857bcab4d2e5a1723d00f3764e0
surya27620 <suryaprakash1463@gmail.com>
2021-05-30 10:24:39 +0530

    just push

---
+++ src/org/project2/Java.java
@@ -0,0 +1,15 @@
+package org.project2;
+
+public class Java {
+
+	private void empId() {
+		System.out.println("enter the name surya");
+
+	}
+	public static void main(String[] args) {
+		Java j = new Java();
+		j.empId();
+	}
+	
+	
+}
